3b36b6086a35a0ee18f8ebff4ff3ba0c3e6ca415
Armkas <113833554+Armkas@users.noreply.github.com>
2023-01-17 15:31:15 +0900

    Update Cell.swift
    尝试异步下载图片

--- flickrFeed/flickrFeed/View/Cell/Cell.swift
+++ flickrFeed/flickrFeed/View/Cell/Cell.swift
@@ -17,7 +17,20 @@ class Cell: UITableViewCell {
         
     }
     
-    func configure(_ item: Item) {
-        self.imageView1.image = UIImage(url: item.media.m)
+    func configure(_ item: Item, callback: @escaping(() -> Void)) {
+        DispatchQueue.global().async{//先转到后台线程,下载好了在转回main线程
+            do{
+                let imageData = try Data(contentsOf: item.media.m)
+                let image = UIImage(data: imageData)//得到图片实例后才转main线程
+                
+                DispatchQueue.main.async {
+                    self.imageView1.image = image
+                    self.imageView1.sizeToFit()
+                    callback()
+                }
+            } catch{
+                print(error)
+            }
+        }
     }
 }
